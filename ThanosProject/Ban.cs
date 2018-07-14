d0df95aba060144ccc6904660f3362b175dc0013
Pharoah C <pharoahcampbell@yahoo.com>
2018-07-14 12:14:23 -0400

    Program completed.

---
+++ ThanosProject/Ban.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThanosProject
+{
+    class Ban
+    {
+        List<string> BanList = new List<string>();
+        List<int> RandomNumbers = new List<int>();
+        Random random = new Random();
+        bool canPass = false;
+        int randomNumber = 0;
+        public List<string> BalanceUniverse(List<string> namePool)
+        {
+            int universeSize = namePool.Count;
+            int banSize = universeSize / 2;
+            string[] universe = new string[universeSize];
+            namePool.CopyTo(universe);
+            for(int i = 0; i < banSize; i++)
+            {
+                //ex. this will run 2 times and pick to different people
+                while(canPass == false)
+                {
+                    randomNumber = random.Next(0, universeSize);
+                    if (RandomNumbers.Contains(randomNumber))
+                    {
+                        canPass = false;
+                    }
+                    else
+                    {
+                        RandomNumbers.Add(randomNumber);
+                        canPass = true;
+                    }
+                }
+                BanList.Add(universe[randomNumber]);
+                canPass = false;
+            }
+            return BanList;
+        }
+    }
+}
--- ThanosProject/Program.cs
+++ ThanosProject/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace ThanosProject
 {
@@ -10,7 +11,29 @@ namespace ThanosProject
     {
         static void Main(string[] args)
         {
-            
+            string universeSizeAsString, names;
+            int universeSizeAsInt;
+            List<string> peopleInUniverse = new List<string>();
+            List<string> bannedPeople = new List<string>();
+            Ban ban = new Ban();
+            Console.WriteLine("How many people is in your universe?");
+            universeSizeAsString = Console.ReadLine();
+            universeSizeAsInt = Convert.ToInt32(universeSizeAsString);
+            for(int i = 0; i < universeSizeAsInt; i++)
+            {
+                Console.WriteLine("Enter a person's name from your universe.");
+                names = Console.ReadLine();
+                peopleInUniverse.Add(names);
+            }
+            bannedPeople = ban.BalanceUniverse(peopleInUniverse);
+            Console.WriteLine("You all stand before Thanos awaiting your faith\nHe snaps");
+            System.Threading.Thread.Sleep(3000);
+            for(int i = 0; i < bannedPeople.Count; i++)
+            {
+                Console.WriteLine(bannedPeople.ElementAt(i));
+            }
+            Console.WriteLine("These souls had to be sacrificed to balance your universe");
+            Console.ReadKey();
         }
     }
 }
